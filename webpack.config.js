59eaf1e2542555ff7e0c19b8041dcea45f37d69e
Jake Helme <jake.helme@gmail.com>
2016-11-13 17:29:33 +0200

    Connected everything to React components. Refactored some things. Added Redux DevTools config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = {
+var htmlWebpackPluginConfig = {
     template: __dirname + '/src/index.html',
     filename: 'index.html',
     inject: 'body'
@@ -19,14 +20,21 @@ module.exports = {
             {
                 loader: 'babel-loader',
                 include: [
-                    path.resolve(__dirname,'/src')
+                    path.resolve(__dirname,'src')
                 ],
                 test: /\.jsx?$/,
                 query:{
-                    presets: ['es2015', 'react']
+                    presets: ['react', 'es2015']
                 }
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin(HtmlWebpackPluginConfig)]
+    plugins: [
+        new HtmlWebpackPlugin(htmlWebpackPluginConfig),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 };
